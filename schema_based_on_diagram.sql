1830dec4030602aab87e8f4ee4852ebddb53f7d7
suleiman gacheru <dretrevor8@gmail.com>
2023-09-26 18:45:53 +0300

    create tables schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,43 @@
+create TABLE patients (
+    id serial primary key,
+    name varchar(50),
+    date_of_birth date
+)
+
+create TABLE medical_histories (
+    id serial primary key,
+    admitted_at timestamp,
+    patient_id int,
+    status varchar(70)
+    constraint fk_patients
+    foreign key (patient_id)
+    references patients(id)
+)
+
+
+create TABLE invoices (
+    total_amount decimal ,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id int,
+    constraint fk_medical_histories
+    foreign key(medical_history_id)
+    references medical_histories(id)
+)
+
+
+create TABLE invoices_items (
+    id serial primary key,
+    unit_price decimal,
+    quantity int,
+    total_price decimal,
+    invoice_id int,
+    treatment_id int,
+)
+
+create TABLE treatments (
+    id serial primary key,
+    type varchar(50),
+    name varchar(35)
+)
 
